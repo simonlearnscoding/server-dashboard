63ea3bc0a10b1fc923caa51cf675b256d3bc9f47
simonlearnscoding <simon.muscas@gmail.com>
2025-04-04 00:57:33 +0200

    type err

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 
 export async function GET(request: Request) {
-  const { userId } = auth();
+  const { userId } = await auth();
   if (!userId) return NextResponse.redirect("/sign-in");
 
   const { searchParams } = new URL(request.url);
